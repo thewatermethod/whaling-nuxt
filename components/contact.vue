21e1aff486d55e3c3315fed2b085733fe29f8833
Matt Bevilacqua <mbevilac@providence.edu>
2018-11-05 12:51:08 -0500

    adding some spam handling per netlify

--- components/contact.vue
+++ components/contact.vue
@@ -7,6 +7,8 @@
         <input type="text" id="input-name" placeholder="Name" name="contactName"> 
         <input type="email" id="input-email" placeholder="Email address" name="contactEmail"> 
        
+        <input type="text" id="input-last-name" placeholder="Last Name" name="lastName" netlify-honeypot="bot-field">
+
         <select id="input-subject" placeholder="Subject" name="contactSubject">
             <option value="Question">I have a question</option>
             <option value="Meeting">I would like to schedule a meeting</option>
@@ -23,57 +25,12 @@
 
 </template>
 
-<script>
-export default {
-  data: function() {
-    return {};
-  },
-
-  methods: {
-    gatherData: function() {
-      return {
-        name: document.querySelector("#input-name").value,
-        returnEmail: document.querySelector("#input-email").value,
-        selection: document.querySelector("#input-subject").value,
-        message: document.querySelector("#input-message").value
-      };
-    },
-
-    sendAjax: function(data) {
-      var url =
-        "https://whalincityweb.azurewebsites.net/api/HttpTriggerJS1?code=A3T5F0vkqQsV9P5MPbPFynewIy96eBg9cD1Zr81fMYW2k8LYDxaHwA==&name=Matt";
-
-      fetch(url, {
-        method: "post",
-        body: data,
-        mode: "no-cors"
-      }).then(response => console.log(response));
-
-      /*.then( response=> response.json() )
-                .then( theJson => console.log(theJson) );*/
-    },
-
-    thankYou: function(event) {
-      //  event.preventDefault();
-      event.target.style.display = "none";
-      var parent = event.target.parentNode;
-      var thankYou = document.createElement("p");
-      thankYou.innerText =
-        "Thanks for your interest... we'll get back to you shortly";
-      parent.appendChild(thankYou);
-    },
-
-    triggerAjax: function() {
-      var data = this.gatherData();
-      this.sendAjax(data);
-    }
-  },
-
-  props: []
-};
-</script>
-
 <style scoped>
+input[name="lastName"] {
+  position: fixed;
+  left: -999999999px;
+}
+
 input,
 select,
 textarea {
